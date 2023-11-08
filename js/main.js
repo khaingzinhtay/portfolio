b4ede522521a4e7e82b00ab199193cbb6f64e0dd
Khaing Zin Htay <ms.khaingzin2003@gmail.com>
2023-11-09 05:49:35 +0630

    Portfolio Website

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+ScrollReveal({ 
+    // reset: true ,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+
+ScrollReveal().reveal('.home-content,.heading', { origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form', { origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img', { origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content', { origin:'right'});
+
+
+const typed = new Typed('.multiple-text',{
+    strings:['Backend Developer','Web Designer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+})
